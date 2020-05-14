85885c171c4ee61ab430b5ed8bd5e28931481987
Miguel Carmona <miguelcarmona996@gmail.com>
2020-05-14 13:38:52 -0400

    Changes in index.js

--- components/Layout/Content.js
+++ components/Layout/Content.js
@@ -3,8 +3,9 @@ import { node, string, oneOfType } from 'prop-types';
 import { Layout } from 'antd';
 
 const AppContent = Layout.Content;
+// eslint-disable-next-line react/jsx-props-no-spreading
 const Content = ({ children, ...props }) => <AppContent {...props}>{children}</AppContent>;
 Content.propTypes = {
- children: oneOfType([node, string]),
+    children: oneOfType([node, string]),
 };
 export default Content;
